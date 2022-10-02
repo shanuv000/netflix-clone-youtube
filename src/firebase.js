9c46c94b0e2053804dbb39724aaad2392fe9665c
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2022-10-02 12:31:17 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsr2HiCSHEGAAYjWAmOLze0xUhvBniXfM",
+  authDomain: "netflix-clone-dbc42.firebaseapp.com",
+  projectId: "netflix-clone-dbc42",
+  storageBucket: "netflix-clone-dbc42.appspot.com",
+  messagingSenderId: "581906095979",
+  appId: "1:581906095979:web:6b31e62d44e404aa9a362e",
+};
